118e4e21cef0c7b5e72e41af9b7a92af8b7e02c9
Carlos Eduardo Vieira <pcfeduardo@gmail.com>
2022-02-11 14:07:05 -0300

    feat: backend into s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,20 @@
 provider "aws" {
-  region  = "us-east-2"
+  region = "us-east-2"
+}
+
+terraform {
+  required_version = ">= 1.0.0"
+
+  backend "s3" {
+    bucket = "tf-demo-states"
+    key    = "tf-remote-example.tfstate"
+    region = "us-east-2"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
