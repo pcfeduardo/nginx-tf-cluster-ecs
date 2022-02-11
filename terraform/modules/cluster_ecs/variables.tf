c0f37a64190dae545b0b55aa7caf52eb591f12b6
Carlos Eduardo Vieira <pcfeduardo@gmail.com>
2022-02-11 14:19:26 -0300

    feat: setting variables

--- terraform/modules/cluster_ecs/variables.tf
+++ terraform/modules/cluster_ecs/variables.tf
@@ -1,20 +1,37 @@
 variable "container_name" {
-  type = string
+  type        = string
   description = "Enter the name of the container to be used by the task definition"
-  default = "nginx-alpine"
+  default     = "nginx-alpine"
 }
 
 variable "vpc_id" {
   type        = string
   description = "Enter the VPC id to be used by the ECS service"
-}
-
-variable "subnet" {
-  type = string
-  description = "Inform the subnet to be used in the ECS service"
+  default     = ""
 }
 
 variable "image_name" {
-  type = string
+  type        = string
   description = "Enter the image address"
+  default     = ""
+}
+
+variable "subnets" {
+  type        = map(string)
+  description = "Inform the subnet to be used in the ECS"
+  default = {
+    main    = ""
+    subnet1 = ""
+    subnet2 = ""
+    subnet3 = ""
+  }
+}
+
+variable "route53_cfg" {
+  type        = map(string)
+  description = "FQDN to ALB"
+  default = {
+    zone_id    = ""
+    dns_record = ""
+  }
 }
