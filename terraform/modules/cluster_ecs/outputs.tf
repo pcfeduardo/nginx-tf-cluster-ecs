22afc83e5f075fa6dfe2978f952ca686117ca158
Carlos Eduardo Vieira <pcfeduardo@gmail.com>
2022-02-11 14:08:20 -0300

    feat: exporting outputs from modules

--- terraform/modules/cluster_ecs/outputs.tf
+++ terraform/modules/cluster_ecs/outputs.tf
@@ -2,3 +2,8 @@ output "alb" {
   value       = aws_lb.alb_nginx_alpine
   description = "All outputs from ALB"
 }
+
+output "alb_cname" {
+  value       = aws_route53_record.alb_nginx_dns
+  description = "All outputs from Route53"
+}
