2b5da817830e2d40ba81270b8532824868ce95b6
Carlos Eduardo Vieira <pcfeduardo@gmail.com>
2022-02-11 11:10:39 -0300

    feat: security group to ecs and alb

---
+++ terraform/modules/cluster_ecs/sg_alb.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "allow_http_from_internet" {
+  name        = "allow-http-from-internet"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "Allow HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+}
+
+
---
+++ terraform/modules/cluster_ecs/sg_ecs.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "allow_http_from_sg_alb" {
+  name        = "allow-http-from-sg-alb"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "Allow HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    security_groups      = [ aws_security_group.allow_http_from_internet.id ]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+}
